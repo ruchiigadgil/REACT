085bf1673ba2a99a42fffdf0f009f730eae9fafe
ruchiigadgil <ruchigadgil@gmail.com>
2024-07-29 10:52:55 +0000

    29_july_16:22

---
+++ first-react-appln/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ClassComponent from './components/ClassComponent';
+import FunctionalComponent from './components/FunctionalComponents';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello World</h1>
+    <ClassComponent />
+    <FunctionalComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-react-appln/src/components/FunctionalComponents.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function FunctionalComponent(){
+    return(
+        <div>
+        <h2>This is my functional component</h2>
+        </div>
+    )
+}
+
+export default FunctionalComponent;
